90462653296cc043d73e96c4b9fe05d2bae63fdf
55tbtb6966-svg <55tbtb6966@privaterelay.appleid.com>
2026-08-21 21:00:42 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="english-qr-reader-v398-20260821114848";
+const CACHE="english-qr-reader-v400-20260821120006";
 self.addEventListener("install",()=>self.skipWaiting());
 self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
 self.addEventListener("fetch",e=>{if(e.request.mode==="navigate")e.respondWith(fetch(e.request,{cache:"no-store"}));});
